9041e4a60414aa220347d3e9be397fdec02f28d8
Exyrus <97399129+Exyrus@users.noreply.github.com>
2022-11-05 21:48:43 +0000

    removed weird files

--- src/implementation/kernel/main.c
+++ src/implementation/kernel/main.c
@@ -1,8 +1,7 @@
 #include "print.h"
-extern getch;
 
 void kernel_main() {
     print_clear();
     print_set_colour(PRINT_COLOUR_WHITE, PRINT_COLOUR_BLACK);
-    print_str("From one OS to another...\nFinestOS.\nInput a command\n> ");
+    print_str("From one OS to another...\nOasisOS.\nInput a command\n> ");
 }
